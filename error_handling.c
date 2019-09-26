1d1d3cdb63c881107d53969dafef02c847a687eb
mrdoomus <mrdoom.official@gmail.com>
2019-09-26 17:45:23 +0000

    Changed cant' for can't

--- error_handling.c
+++ error_handling.c
@@ -21,25 +21,25 @@ void error_handling(char *id, unsigned int line_number)
 	if (!strcmp("push", id))
 		print_error(line_number, "usage: push integer");
 	else if (!strcmp("pint", id))
-		print_error(line_number, "cant' pint, stack empty");
+		print_error(line_number, "can't pint, stack empty");
 	else if (!strcmp("pop", id))
 		print_error(line_number, "can't pop an empty stack");
 	else if (!strcmp("swap", id))
 		print_error(line_number, "can't swap, stack too short");
 	else if (!strcmp("add", id))
-		print_error(line_number, "cant' add, stack too short");
+		print_error(line_number, "can't add, stack too short");
 	else if (!strcmp("sub", id))
-		print_error(line_number, "cant' sub, stack too short");
+		print_error(line_number, "can't sub, stack too short");
 	else if (!strcmp("div", id))
-		print_error(line_number, "cant' div, stack too short");
+		print_error(line_number, "can't div, stack too short");
 	else if (!strcmp("mod", id))
-		print_error(line_number, "cant' mod, stack too short");
+		print_error(line_number, "can't mod, stack too short");
 	else if (!strcmp("divzero", id))
 		print_error(line_number, "division by zero");
 	else if (!strcmp("mul", id))
-		print_error(line_number, "cant' mul, stack too short");
+		print_error(line_number, "can't mul, stack too short");
 	else if (!strcmp("pchar", id))
-		print_error(line_number, "cant' pchar, stack empty");
+		print_error(line_number, "can't pchar, stack empty");
 	else if (!strcmp("pcharout", id))
-		print_error(line_number, "cant' pchar, value out of range");
+		print_error(line_number, "can't pchar, value out of range");
 }
